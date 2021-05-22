e9f288acfaf66aa33146d600286aa1af132eaa11
Lavanya Jain <Lavanya.pjain@gmail.com>
2021-05-22 20:29:38 +0600

    Initial commit

---
+++ src/main/java/com/intuit/interview/businessprofile/businessprofile/BusinessProfileApplication.java
@@ -0,0 +1,13 @@
+package com.intuit.interview.businessprofile.businessprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intuit/interview/businessprofile/businessprofile/BusinessProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.interview.businessprofile.businessprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
